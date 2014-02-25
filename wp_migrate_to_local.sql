e1f6423571d3b47967327311f3e297df10c616a2
Florian Heinle <github@planet-tiax.de>
2014-02-25 23:59:14 +0100

    demo

--- wp_migrate_to_local.sql
+++ wp_migrate_to_local.sql
@@ -1,3 +1,3 @@
-UPDATE wp_options SET option_value = 'http://localhost/wordpress/' where option_name = 'siteurl';
-UPDATE wp_options SET option_value = 'http://localhost/wordpress/' where option_name = 'home';
-UPDATE wp_posts SET post_content = REPLACE(post_content, 'staging.example.com', 'localhost');
+UPDATE wp_options SET option_value = 'http://debian.local' where option_name = 'siteurl';
+UPDATE wp_options SET option_value = 'http://debian.local' where option_name = 'home';
+UPDATE wp_posts SET post_content = REPLACE(post_content, 'http://demo.florianheinle.de', 'http://debian.local');
